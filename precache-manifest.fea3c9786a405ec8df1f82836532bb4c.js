51e8644e021b7b96e43e15c1866b7e8a8a423e7c
Macie Grant <niko247742@yahoo.com>
2019-05-06 22:55:22 -0400

    Updates

---
+++ precache-manifest.fea3c9786a405ec8df1f82836532bb4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe524f848ead8c67d40ac36f71d78fe",
+    "url": "/Reactjs-Basics/index.html"
+  },
+  {
+    "revision": "597a99c4a268b9ad7895",
+    "url": "/Reactjs-Basics/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Reactjs-Basics/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "597a99c4a268b9ad7895",
+    "url": "/Reactjs-Basics/static/js/main.8a331b3a.chunk.js"
+  },
+  {
+    "revision": "018ef0a7cee1032a0319",
+    "url": "/Reactjs-Basics/static/js/runtime~main.93b9aca1.js"
+  }
+]);
